39f597c258c97220d87cb5456b8ce90972187be4
Yovany Cunha <yovany33@gmail.com>
2024-09-12 21:11:46 -0300

    adjust professional model - sprint 12

--- pkg/models/professional.model.go
+++ pkg/models/professional.model.go
@@ -4,11 +4,13 @@ type Professional struct {
 	Name 			string 		`json:"name" bson: "professional_name"`
 	Document 		string 		`json:"document" bson: "professional_document"`
 	Specialty		string 		`json:"specialty" bson: "professional_specialty"`
+	IsCertified		bool 		`json:"iscertified" bson: "professional_is_certified"`
 }
 
 type ProfessionalUpdate struct {
 	Name 			string 		`json:"name" bson: "professional_name"`
 	Specialty		string 		`json:"specialty" bson: "professional_specialty"`
+	IsCertified		bool 		`json:"iscertified" bson: "professional_is_certified"`
 	NewDocument		string 		`json:"newdocument" bson: "professional_new_document"`
 	OldDocument		string 		`json:"olddocument" bson: "professional_old_document"`
 }
