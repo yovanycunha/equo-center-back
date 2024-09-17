062e36a838134ab9b5089c25d6b10029d28c547a
Yovany Cunha <yovany33@gmail.com>
2024-09-16 23:00:39 -0300

    update activity model - sprint 13

--- pkg/models/activity.model.go
+++ pkg/models/activity.model.go
@@ -3,9 +3,10 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Activity struct {
-	ID primitive.ObjectID `json:"id" bson:"_id, omitempty"`
-	Purpose string `json:"purpose" bson:"purpose"`
-	Professionals []string `json:"professionals" bson:"professionals"`
-	Actions string `json:"actions" bson:"actions"`
-	Feedback string `json:"feedback" bson:"feedback"`
+	ID 				primitive.ObjectID 	`json:"id" bson:"_id, omitempty"`
+	Title 			string 				`json:"title" bson:"title"`
+	Purpose 		string 				`json:"purpose" bson:"purpose"`
+	Professionals 	[]string 			`json:"professionals" bson:"professionals"`
+	Actions 		string 				`json:"actions" bson:"actions"`
+	Feedback 		string 				`json:"feedback" bson:"feedback"`
 }
